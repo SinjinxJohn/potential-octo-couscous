2a6da769f697073bee0da85b19cac9964c4fb619
sinjinxjohn <sinjin.hotlinebling@gmail.com>
2023-02-07 22:11:55 +0530

    added homePage

---
+++ frontend/lib/HomePage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        Container(
+          child: Row(children: [
+            Column(
+              children: [
+                Text("Hello Emmanuel,", style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                Text('What are you buying today?',style: TextStyle(fontSize: 20),)
+              ],
+            ),
+            Spacer(),
+            Container(
+              child: Icon(Icons.shopping_bag),
+            )
+          ]),
+        )
+      ]),
+    );
+  }
+}
