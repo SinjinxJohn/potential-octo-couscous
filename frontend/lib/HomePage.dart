c2895871a2729e825d36aaf31e2fa4a540a2f189
sinjinxjohn <sinjin.hotlinebling@gmail.com>
2023-02-09 00:08:26 +0530

    did some api integration in gridview

--- frontend/lib/HomePage.dart
+++ frontend/lib/HomePage.dart
@@ -3,6 +3,8 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:frontend/components/gridview.dart';
 import 'package:frontend/components/searchbar.dart';
+
+import 'data/category.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -11,6 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Category category = Category();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +45,37 @@ padding: EdgeInsets.all(15),
             Text("Trending Sales",style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold) ,),
 
             SizedBox(height: 20,),
-            GridB()
+            Container(
+              child: FutureBuilder<List>(future: category.getAllCategories(),builder: (context,snapshot){
+                // print(snapshot.data);
+                if(snapshot.hasData){
+                  return GridView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: snapshot.data!.length,
+      
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(mainAxisExtent: 300,crossAxisCount: 2,crossAxisSpacing: 12.0,mainAxisSpacing: 12.0), itemBuilder: (_,index){
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(14),
+            color: Color.fromARGB(255, 201, 218, 227)
+          ),
+
+          child: Column(
+            children: [
+              Image.network("${snapshot.data?.elementAt(index)['image']}",height: 200,)
+            ],
+          ),
+          // color: Colors.purpleAccent,
+        );
+
+      });;
+                }else{
+                  return Text('No data found');
+                }
+              }, ),
+            )
           
             
             ],),
--- frontend/lib/components/gridview.dart
+++ frontend/lib/components/gridview.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:frontend/data/category.dart';
 
 class GridB extends StatefulWidget {
-  const GridB({super.key});
+  final int length;
+  const GridB({super.key,required this.length});
 
   @override
   State<GridB> createState() => _GridBState();
 }
 
 class _GridBState extends State<GridB> {
-  Category category = Category();
+  Category category1=Category();
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
       physics: NeverScrollableScrollPhysics(),
       shrinkWrap: true,
-      itemCount: 8,
+      itemCount: 3,
       
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(mainAxisExtent: 250,crossAxisCount: 2,crossAxisSpacing: 12.0,mainAxisSpacing: 12.0), itemBuilder: (_,idex){
         return Container(
@@ -26,6 +28,7 @@ class _GridBState extends State<GridB> {
             borderRadius: BorderRadius.circular(14),
             color: Color.fromARGB(255, 201, 218, 227)
           ),
+          // child: ,
           // color: Colors.purpleAccent,
         );
 
