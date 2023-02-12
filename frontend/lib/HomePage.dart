fbac6de81202e554e99d52326686fa5eb0010516
sinjinxjohn <sinjin.hotlinebling@gmail.com>
2023-02-13 00:29:07 +0530

    added details page

--- frontend/lib/HomePage.dart
+++ frontend/lib/HomePage.dart
@@ -50,6 +50,7 @@ class _HomePageState extends State<HomePage> {
     SizeConfig().init(context);
     return SafeArea(
       child: Scaffold(
+          backgroundColor: Color.fromARGB(255, 249, 243, 240),
           appBar: appBar(),
           body: SingleChildScrollView(
             child: Padding(
@@ -62,9 +63,11 @@ class _HomePageState extends State<HomePage> {
                       // mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        SizedBox(
-                          height: 20,
-                        ),
+                        // searchBar(text: "Enter the product name"),
+                        // SizedBox(
+                        //   height: 20,
+                        // ),
+                        
                         Text(
                           "Browse by category",
                           style: TextStyle(
@@ -105,12 +108,12 @@ class _HomePageState extends State<HomePage> {
                             height: SizeConfig.defaultSize * 5,
                             width: SizeConfig.defaultSize * 5,
                             decoration: BoxDecoration(
-
+          
                                 // boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5),spreadRadius: 2,blurRadius: 1)],
                                 // s
                                 borderRadius: BorderRadius.circular(14),
                                 color: Color.fromARGB(255, 233, 232, 232)),
-
+          
                             child: Center(
                               child: Column(
                                 children: [
@@ -130,7 +133,7 @@ class _HomePageState extends State<HomePage> {
                                                     loadingProgress) {
                                           if (loadingProgress == null)
                                             return child;
-
+          
                                           return Center(
                                               child: Lottie.asset(
                                                   "/home/sinjin/ecommerce/frontend/assets/79609-loading-button.json"));
@@ -154,13 +157,13 @@ class _HomePageState extends State<HomePage> {
                           ),
                         );
                       }),
-
+          
                   //     else {
                   //       return Lottie.network('https://assets4.lottiefiles.com/datafiles/QeC7XD39x4C1CIj/data.json');
                   //     }
                   //   },
                   // ),
-
+          
                   // const searchBar(text: "Search Products"),
                   // const SizedBox(
                   //   height: 15,
@@ -168,62 +171,6 @@ class _HomePageState extends State<HomePage> {
                   SizedBox(
                     height: SizeConfig.defaultSize * 2,
                   ),
-                  //  Text(
-                  //   "Trending Sales",
-                  //   style: TextStyle(fontSize: SizeConfig.defaultSize*1.6, fontWeight: FontWeight.bold),
-                  // ),
-                  // const SizedBox(
-                  //   height: 20,
-                  // ),
-                  // FutureBuilder<List>(
-                  //   future: _category.getAllCategories(),
-                  //   builder: (context,snapshot){
-                  //     if(snapshot.hasData){
-                  //       return PageView.builder(
-                  //         itemCount: snapshot.data!.length,
-                  //         itemBuilder: ((context,index){
-                  //           return Container(
-                  //               height: SizeConfig.defaultSize*5,
-                  //               width: SizeConfig.defaultSize*5,
-                  //               decoration: BoxDecoration(
-
-                  //                 // boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5),spreadRadius: 2,blurRadius: 1)],
-                  //                   // s
-                  //                   borderRadius: BorderRadius.circular(14),
-                  //                   color: Color.fromARGB(255, 233, 232, 232)),
-
-                  //               child: Center(
-                  //                 child: Column(
-                  //                   children: [
-                  //                     Padding(
-                  //                       padding: EdgeInsets.only(bottom:SizeConfig.defaultSize*1.6),
-                  //                       child: ClipRRect(
-                  //                         borderRadius: BorderRadius.only(topLeft: Radius.circular(16),topRight: Radius.circular(16)),
-                  //                         child: Image.network(
-                  //                           "${snapshot.data?.elementAt(index)['image']}",
-                  //                           fit: BoxFit.cover,
-                  //                           width: double.infinity,
-                  //                           height: 260,
-                  //                           // height: SizeConfig.screenHeight*0.27,
-                  //                           // width: SizeConfig.screenWidth*0.9,
-                  //                         ),
-                  //                       ),
-                  //                     ),
-                  //                     // SizedBox(height: SizeConfig.defaultSize*0.1),
-                  //                     Text(snapshot.data!
-                  //                         .elementAt(index)['name'],style: TextStyle(fontSize: SizeConfig.defaultSize*1.1,color: kTextColor,fontWeight: FontWeight.bold),),
-
-                  //                   ],
-                  //                 ),
-                  //               ),
-                  //               // color: Colors.purpleAccent,
-                  //             );
-
-                  //         }));
-                  //     }else{
-                  //       return Text("No data available");
-                  //     }
-                  //   })
                 ],
               ),
             ),
--- frontend/lib/detailsPage.dart
+++ frontend/lib/detailsPage.dart
@@ -7,6 +7,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:frontend/HomePage.dart';
+import 'package:frontend/components/constants.dart';
 import 'package:frontend/data/category.dart';
 import 'package:frontend/data/categoryAPI.dart';
 import 'package:frontend/data/productAPI.dart';
@@ -21,9 +22,59 @@ class DetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 249, 243, 240),
       appBar: appbarDetail(context),
 
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(product.image.toString(),
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: 400,
+            ),
+            SizedBox(
+              height: SizeConfig.defaultSize*2,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left:8),
+                  child: Text(product.name!.toUpperCase().toString(),style: TextStyle(fontSize: SizeConfig.defaultSize*1.6,fontWeight: FontWeight.bold),),
+                ),
+                Spacer(),
+                 Padding(
+              padding: const EdgeInsets.only(left:8.0,right: 8),
+              child: Text("\$"+product.price.toString(),style: TextStyle(fontSize: SizeConfig.defaultSize*1.6,fontWeight: FontWeight.bold,color: Color.fromARGB(255, 81, 80, 80)),),
+            ),
+              ],
+            ),
+            SizedBox(height: SizeConfig.defaultSize*1.6,),
+            Padding(
+              padding: const EdgeInsets.only(left:8.0,right:8),
+              child: Text(product.desc.toString(),style: TextStyle(fontSize: SizeConfig.defaultSize*1.3,color: kTextColor),),
+            ),
+            // SizedBox(height: SizeConfig.defaultSize*1,),
+           
+            SizedBox(height: SizeConfig.defaultSize*7,),
+            // Spacer(),
+            Center(
+              child: Container(
+                height:SizeConfig.defaultSize*5 ,
+                width: SizeConfig.screenWidth*0.8,
+                decoration: BoxDecoration(color: kPrimaryColor,borderRadius: BorderRadius.circular(18)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left:30.0, right: 30),
+                  child: Center(child: Text("ADD TO CART",style: TextStyle(fontSize: SizeConfig.defaultSize*1.3,fontWeight: FontWeight.bold),)),
+                ),
+              ),
+            )
+            
+        
+          ],
+        ),
+      ),
     );
   }
 
@@ -35,6 +86,7 @@ class DetailsPage extends StatelessWidget {
           icon: Image.asset(
             "/home/sinjin/ecommerce/frontend/assets/icons8-shopping-cart-24.png",
             height: SizeConfig.screenHeight* 0.5,
+            
           ),
         )
       ],
@@ -42,8 +94,8 @@ class DetailsPage extends StatelessWidget {
           onPressed: () {
             Navigator.of(context).pop();
           },
-          icon: Image.asset(
-            "/home/sinjin/ecommerce/frontend/assets/icons8-back-arrow-30.png",
+          icon: SvgPicture.asset(
+            "/home/sinjin/ecommerce/frontend/assets/back-arrow-direction-down-right-left-up-2-svgrepo-com.svg",
             height: SizeConfig.defaultSize * 2,
           )),
     );
