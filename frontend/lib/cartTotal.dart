1b46d6011a5eeaa1f9d101f55b54d7384f7f6b5a
sinjinxjohn <sinjin.hotlinebling@gmail.com>
2023-02-17 21:27:48 +0530

    added checkout button

--- frontend/lib/cartTotal.dart
+++ frontend/lib/cartTotal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:frontend/components/cartController.dart';
@@ -11,23 +12,49 @@ class CartTotal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(()=>
-       Container(
-        
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 75,),
-          child: Row(
+    return Obx(
+      () => Container(
+        child: Container(
+          height: 150,
+          width: 480,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(15)),
+          child: Column(
             children: [
-              Text("Subtotal:", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.only(right:30.0),
-                child: Text("\$${_controller.Total}", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 50.0,top: 15),
+                    child: Text(
+                      "Subtotal:",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 50.0,top: 15),
+                    child: Text(
+                      "\$${_controller.Total}",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  // IconButton(onPressed: (){}, icon: i)
+                ],
               ),
+              SizedBox(height: 30,),
+              TextButton(onPressed: () {}, child: Container(
+                height: 50,
+                width: 400,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(12)
+                ),
+                child: Center(child: Text("Check Out",style: TextStyle(color: Colors.white),))))
             ],
           ),
         ),
-    
       ),
     );
   }
